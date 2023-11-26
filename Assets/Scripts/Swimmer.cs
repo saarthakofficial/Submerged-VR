4f02e6cdbb6d67a1448205b1f026633a169275a0
Saarthak Srivastava <saarthak2k@gmail.com>
2023-11-26 13:05:33 +0530

    added ladder and seperated control system

--- Assets/Scripts/Swimmer.cs
+++ Assets/Scripts/Swimmer.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.TextCore.Text;
+using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
 
 [RequireComponent(typeof(Rigidbody))]
 public class Swimmer : MonoBehaviour
@@ -10,37 +12,85 @@ public class Swimmer : MonoBehaviour
     [SerializeField] float dragForce = 1f;
     [SerializeField] float minForce;
     [SerializeField] float minTimeBetweenStrokes;
+    [SerializeField] float maxSwimHeight = 5f; // Adjust this value to set the maximum swim height
     [SerializeField] InputActionReference leftControllerSwimReference;
     [SerializeField] InputActionReference leftControllerVelocity;
     [SerializeField] InputActionReference rightControllerSwimReference;
     [SerializeField] InputActionReference rightControllerVelocity;
     [SerializeField] Transform trackingTransform;
     Rigidbody rb;
+    [SerializeField] DynamicMoveProvider moveProvider;
+    [SerializeField] CharacterController cc;
     float cooldownTimer;
 
-    void Awake(){
+    void Awake()
+    {
         rb = GetComponent<Rigidbody>();
-        rb.useGravity = false;
     }
 
-    void FixedUpdate(){
-        cooldownTimer += Time.fixedDeltaTime;
-        if (cooldownTimer > minTimeBetweenStrokes && leftControllerSwimReference.action.IsPressed() && rightControllerSwimReference.action.IsPressed()){
-            var leftHandVelocity = leftControllerVelocity.action.ReadValue<Vector3>();
-            var rightHandVelocity = rightControllerVelocity.action.ReadValue<Vector3>();
-            Vector3 localVelocity = leftHandVelocity + rightHandVelocity;
-            localVelocity *= -1;
-
-            if (localVelocity.sqrMagnitude > minForce * minForce){
-                Vector3 worldVelocity = trackingTransform.TransformDirection(localVelocity);
-                rb.AddForce(worldVelocity * swimForce, ForceMode.Acceleration);
-                cooldownTimer = 0f;
+    void FixedUpdate()
+    {
+            if (transform.position.y < maxSwimHeight){
+                rb.useGravity = false;
             }
-        }
+            else{
+                rb.useGravity = true;
+            }
 
-        if (rb.velocity.sqrMagnitude > 0.01f){
-            rb.AddForce(-rb.velocity * dragForce, ForceMode.Acceleration);
+            cooldownTimer += Time.fixedDeltaTime;
+            if (cooldownTimer > minTimeBetweenStrokes && leftControllerSwimReference.action.IsPressed() && rightControllerSwimReference.action.IsPressed())
+            {
+                var leftHandVelocity = leftControllerVelocity.action.ReadValue<Vector3>();
+                var rightHandVelocity = rightControllerVelocity.action.ReadValue<Vector3>();
+                Vector3 localVelocity = leftHandVelocity + rightHandVelocity;
+                localVelocity *= -1;
+
+                if (localVelocity.sqrMagnitude > minForce * minForce)
+                {
+                    Vector3 worldVelocity = trackingTransform.TransformDirection(localVelocity);
+
+                    // Separate the horizontal and vertical components
+                    Vector3 horizontalVelocity = new Vector3(worldVelocity.x, 0, worldVelocity.z);
+                    Vector3 verticalVelocity = new Vector3(0, worldVelocity.y, 0);
+
+                    // Check if the player is below the maximum swim height for vertical movement
+                    if (transform.position.y < maxSwimHeight)
+                    {
+                        rb.AddForce(verticalVelocity * swimForce, ForceMode.Acceleration);  
+                    }
+
+
+                    // Always apply the vertical force for buoyancy
+                        rb.AddForce(horizontalVelocity * swimForce, ForceMode.Acceleration);
+
+                    cooldownTimer = 0f;
+                }
+            }
+
+            if (rb.velocity.sqrMagnitude > 0.01f)
+            {
+                rb.AddForce(-rb.velocity * dragForce, ForceMode.Acceleration);
+            }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Surface"){
+            SurfaceControl();
+        }
+        if (other.gameObject.tag == "Dive"){
+            UnderwaterControl();
         }
     }
 
+    void SurfaceControl(){
+        rb.useGravity = false;
+        cc.enabled = true;
+        moveProvider.enabled = true;
+    }
+
+    void UnderwaterControl(){
+        cc.enabled = false;
+        moveProvider.enabled = false;
+        rb.useGravity = true;
+    }
 }
